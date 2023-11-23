8d60f38c3a26ee2127d57c2491d644a869b8e07b
DoniyorSayfiddinov <sayfiddinovdoniyor833@gmail.com>
2023-11-23 21:22:02 +0500

    python- venv

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import requests
 import json
 
+
 response = requests.get('https://randomuser.me/api/')
-
+    
 if response.status_code == 200:
-    content = response.content
-    data = json.loads(content.decode())
-
-    print(data)
-    print(type(data))
+        content = response.content
+        data = json.loads(content.decode())
+        print(data)
+{
+    "fullname": "fadsfsad",
+    "email":"dffdgdctr",
+    "phone": "fadsfsad",
+    "age":215
+}
